90511f458a0fdba4a2dc05560198fe61840a8435
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2024-10-23 09:01:46 -0500

    Vistas de Equipo mejoradas :)

---
+++ Soledad_Cabrera_Taller_1_deportes/Migrations/20241023130848_MigracionNoSeCual.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Soledad_Cabrera_Taller_1_deportes.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionNoSeCual : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
